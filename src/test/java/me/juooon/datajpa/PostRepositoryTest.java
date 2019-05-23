d870fe85e0f812ad3ffb12033301adbb5f3f4117
HyunjunJeon <jeonhj920@gmail.com>
2019-05-23 10:16:01 +0900

    1. Spring Data JPA - Common

---
+++ src/test/java/me/juooon/datajpa/PostRepositoryTest.java
@@ -0,0 +1,69 @@
+package me.juooon.datajpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PostRepositoryTest {
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    @Rollback(false) // insert 쿼리를 꼭 보고싶다 -> JPA와 Hibernate가 굳이 롤백할 쿼리를 db에 영속화 시키지 않았으므로...
+    public void crudRepository() {
+        // Given
+        Post post = new Post();
+        post.setTitle("Spring Data JPA Class....");
+        assertThat(post.getId()).isNull();
+
+        // When
+        Post savedPost = postRepository.save(post);
+
+        // Then
+        assertThat(savedPost.getId()).isNotNull();
+
+        // When
+        List<Post> posts = postRepository.findAll();
+
+        // Then
+        assertThat(posts.size()).isEqualTo(1);
+        assertThat(posts).contains(savedPost);
+
+        // When
+        Page<Post> page = postRepository.findAll(PageRequest.of(0, 10));
+
+        // Then
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+
+        // 새로운 메서드를 만든 후 (PostRepository에)
+
+        // When
+        page = postRepository.findByTitleContains("Spring Data JPA Class....", PageRequest.of(0, 10));
+
+        // Then
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+
+        // When
+        long spring = postRepository.countByTitleContains("Spring");
+
+        // Then
+        assertThat(spring).isEqualTo(1);
+    }
+}
